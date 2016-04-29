5b2d58d7516b84be6a7ef052366e57c9c3315c4f
iFung5S <shermanfung@Sherman-de-MacBook-Air.local>
2016-04-29 01:10:55 +0100

    Added Java version of Binary Search Tree

---
+++ Binary-Search-Tree/BinarySearchTree.java
@@ -0,0 +1,109 @@
+public class BinarySearchTree<Type extends Comparable<Type>>
+                             implements Comparable<BinarySearchTree<Type>>
+{
+  private boolean empty;
+
+  private Type value;
+  private BinarySearchTree<Type> left;
+  private BinarySearchTree<Type> right;
+
+  public BinarySearchTree()
+  {
+    setEmpty();
+  }
+
+  private void setEmpty()
+  {
+    empty = true;
+    left = null;
+    right = null;
+  }
+
+  public boolean isEmpty()
+  {
+    return empty;
+  }
+
+  public Type getValue()
+  {
+    return value;
+  }
+
+  public BinarySearchTree<Type> getLeft()
+  {
+    return left;
+  }
+
+  public BinarySearchTree<Type> getRight()
+  {
+    return right;
+  }
+
+  private void setValue(Type requiredValue)
+  {
+    if (empty)
+    {
+      empty = false;
+      left  = new BinarySearchTree<Type>();
+      right = new BinarySearchTree<Type>();
+    }
+    value = requiredValue;
+  }
+
+  public void insert(Type insertValue)
+  {
+    if (empty)
+      setValue(insertValue);
+    else if (insertValue.compareTo(value) < 0)
+      left.insert(insertValue);
+    else
+      right.insert(insertValue);
+  }
+
+  public boolean find(Type findValue)
+  {
+    if (empty)
+      return false;
+    else if (findValue == value)
+      return true;
+    else if (findValue.compareTo(value) < 0)
+      return left.find(findValue);
+    else
+      return right.find(findValue);
+  }
+
+  public int getSize()
+  {
+    if (empty)
+      return 0;
+    else
+      return (left.getSize() + 1 + right.getSize());
+  }
+
+  public int getDepth()
+  {
+    if (empty)
+      return 0;
+    else
+    {
+      int leftDepth = left.getDepth();
+      int rightDepth = right.getDepth();
+      if (leftDepth > rightDepth)
+        return leftDepth + 1;
+      else
+        return rightDepth + 1;
+    }
+  }
+
+  @Override
+  public int compareTo(BinarySearchTree<Type> other)
+  {
+    return this.getValue().compareTo(other.getValue());
+  }
+
+  public boolean equals(BinarySearchTree<Type> other)
+  {
+    return this.getValue().equals(other.getValue());
+  }
+
+}
